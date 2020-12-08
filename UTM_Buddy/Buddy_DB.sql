fc5f7fe8b66b10781c94eeeec41e761b054438fd
KJIeu <65282522+KJIeu@users.noreply.github.com>
2020-12-08 21:36:44 +0200

    Add files via upload

---
+++ UTM_Buddy/Buddy_DB.sql
@@ -0,0 +1,53 @@
+create database UTM_Buddy;
+use UTM_Buddy;
+CREATE TABLE GroupTable
+(
+    GroupId INT AUTO_INCREMENT PRIMARY KEY unique,
+	GroupCod VARCHAR(9) not null,
+    GroupSpeciality varchar(50) not null,
+    GroupYear TINYINT UNSIGNED not null
+);
+
+CREATE TABLE UsersTable
+(
+    UserId INT AUTO_INCREMENT PRIMARY KEY unique,
+    UserEmail VARCHAR(30) NOT NULL unique,
+    UserPassword VARCHAR(30) NOT NULL,
+	UserName VARCHAR(50) not null,
+    UserGroupId int not null,
+    foreign key (UserGroupId) references GroupTable (GroupId),
+    UserPhoto VARCHAR(50) unique	#link to the photo in storage
+);
+
+create table TeacherTable
+(
+	TeacherId INT AUTO_INCREMENT PRIMARY KEY unique,
+    TeacherEmail VARCHAR(30) NOT NULL unique,
+	TeacherName VARCHAR(50) not null,
+    TeacherPhoto VARCHAR(50) unique #link to the photo in storage
+);
+
+
+create table Timetable
+(
+	GroupId int not null,
+	foreign key (GroupId) references GroupTable (GroupId),
+    WeekType bool,		#true - even, false - odd, null - for both 
+    Zi char(3) not null,
+    Timp time not null,
+    ObjectName varchar(30),
+    TeacherId int not null,
+    foreign key (TeacherId) references TeacherTable (TeacherId) ,
+    Location char(5) not null
+);
+
+create table AcademicPerformance
+(
+	UserId int primary key not null,
+    ObjectName varchar(30),
+    Midterm1 TINYINT UNSIGNED,
+    Midterm2 TINYINT UNSIGNED,
+    Exam TINYINT UNSIGNED,
+    total decimal(5,3)
+);
+
