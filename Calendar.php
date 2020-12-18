781a074553522a6d0d81a121520b19b302cb253e
Anniegavr <56108881+Anniegavr@users.noreply.github.com>
2020-12-19 00:12:26 +0200

    Update Calendar.php
    Updated server connection and fixed the login button

--- Calendar.php
+++ Calendar.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+    require 'conn.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,31 +18,35 @@
         <p class="homearrowtext"></p>
     </div>
 
-
-
-<table width=40% >
   <nav>
-  <a class ="active" href="index.php">Home</a>
+  <a href="profile.php">Profile</a>
   <a href="Timetable.php">Timetable</a>
   <a href="News.php">News</a>
+  <a class ="active" href="Calendar.php">Calendar</a>
+  <a href="Notes.php">Flashcards</a>
+   <a href="ToDo.php">Notes</a>
+   <?php
+        if(isset($_SESSION["login"]) && $_SESSION["login"] == "yes"){
+            echo "<a href='Logout.php'>Logout</a>";
+        }else{
+            echo "<a href='/'>Login</a>";
+        }
 
-  <a href="Calendar.php" >Calendar</a>
+    ?>
+    
 
-  <a href="Profile.php">Profile</a>
-   <a href="Notes.php">Notes</a>
-
-<div class="animation start-home"></div>
-
-<h1><span class="">Calendar</span></h1>
+    <div class="animation start-calendar"></div>
+    <h1><span class="">Calendar</span></h1>
 </nav>
 
+<table width=40%>
  </ul>
 
 
   <thead>
   <tr>
     <td>«</td>
-    <th colspan=5> <div id="month">Decembrie</div></th>
+    <th colspan=5> <div id="month">January</div></th>
     <td>»</td>
   </tr>
   </thead>
@@ -55,49 +63,58 @@
   </thead>
   <tbody class=daynum id=day>
   <tr>
-    <th>29</th>
-    <th>30</th>
-    <th>1</th>
-    <th>1</th>
-    <th>3</th>
-    <th>4</th>
-    <th class="today">5</th>
-  </tr>
-  <tr>
-    <th>6</th>
-    <th>7</th>
-    <th>8</th>
-    <th>9</th>
-    <th>10</th>
-    <th>11</th>
-    <th>12</th>
-  </tr>
-  <tr>
-    <th>13</th>
-    <th>14</th>
-    <th>15</th>
-    <th>16</th>
-    <th>17</th>
-    <th>18</th>
-    <th>19</th>
-  </tr>
-  <tr>
-    <th>20</th>
-    <th>21</th>
-    <th>22</th>
-    <th>23</th>
-    <th>24</th>
-    <th>25</th>
-    <th>26</th>
-  </tr>
-  <tr>
     <th>27</th>
     <th>28</th>
     <th>29</th>
     <th>30</th>
     <th>31</th>
+    <th>1</th>
+    <th>2</th>
+  </tr>
+  <tr>
+    <th>3</th>
+    <th>4</th>
+    <th>5</th>
+    <th>6</th>
+    <th>7</th>
+    <th>8</th>
+    <th>9</th>
+  </tr>
+  <tr>
+    <th  class="today">10</th>
+    <th>11</th>
+    <th>12</th>
+    <th>13</th>
+    <th>14</th>
+    <th>15</th>
+    <th>16</th>
+  </tr>
+  <tr>
+    <th>17</th>
+    <th>18</th>
+    <th>19</th>
+    <th>20</th>
+    <th>21</th>
+    <th>22</th>
+    <th>23</th>
+  </tr>
+  <tr>
+    <th>24</th>
+    <th>25</th>
+    <th>26</th>
+    <th>27</th>
+    <th>28</th>
+    <th>29</th>
+    <th>30</th>
+  </tr>
+  <tr>
+    <th>31</th>
     <th class="nextdate">1</th>
     <th class="nextdate">2</th>
+    <th class="nextdate">3</th>
+    <th class="nextdate">4</th>
+    <th class="nextdate">5</th>
+    <th class="nextdate">6</th>
   </tr>
   </tbody>
 </table>
