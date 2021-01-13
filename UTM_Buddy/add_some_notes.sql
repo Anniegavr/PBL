2efc2e3329e0aefb18bde1929bef305065e374f7
KJIeu <65282522+KJIeu@users.noreply.github.com>
2021-01-13 14:27:41 +0200

    Add files via upload
    in this file you'll fink some datas for test

---
+++ UTM_Buddy/add_some_notes.sql
@@ -0,0 +1,270 @@
+insert grouptable(GroupCod, GroupSpeciality, GroupYear)
+values ('FAF191','Ingineria Software', 1);
+
+INSERT userstable(UserEmail, UserPassword, UserName, UserGroupId, RegControl)
+values('anastasia.gavrilita@isa.utm.md', 'mein', 'Anastasia Gavrilita', 1, 1);
+
+INSERT userstable(UserEmail, UserPassword, UserName, UserGroupId, RegControl)
+values('ilcenco.eugen@isa.utm.md', 'mein1', 'Ilcenco Eugen', 1, 1);
+
+INSERT userstable(UserEmail, UserPassword, UserName, UserGroupId, RegControl)
+values('boico.alexandr@isa.utm.md', 'mein2', 'Boico Alexandr', 1, 1);
+
+INSERT userstable(UserEmail, UserPassword, UserName, UserGroupId, RegControl)
+values('britschi.daniel@isa.utm.md', 'mein3', 'Britschi Daniel', 1, 1);
+
+INSERT userstable(UserEmail, UserPassword, UserName, UserGroupId, RegControl)
+values('octavian.grecu@isa.utm.md', 'mein4', 'Octavian Grecu', 1, 1);
+
+
+insert grouptable(GroupCod, GroupSpeciality, GroupYear)
+values ('TI191','Technologii Informationale', 1);
+
+INSERT userstable(UserEmail, UserPassword, UserName, UserGroupId, RegControl)
+values('laura.colesnic@ti.utm.md', 'asdfg1234', 'Laura Colesnic', 2, 1);
+
+INSERT userstable(UserEmail, UserPassword, UserName, UserGroupId, RegControl)
+values('marin.cretu@ti.utm.md', 'zxcvb1234', 'Marin Cretu', 2, 1);
+
+INSERT userstable(UserEmail, UserPassword, UserName, UserGroupId, RegControl)
+values('taisia.enache@ti.utm.md', 'lkjhg1234', 'Taisia Enachi', 2, 1);
+
+INSERT userstable(UserEmail, UserPassword, UserName, UserGroupId, RegControl)
+values('gherghe.gogean@ti.utm.md', 'zxcvb1234', 'Gherghe Gogean', 2, 1);
+
+INSERT userstable(UserEmail, UserPassword, UserName, UserGroupId, RegControl)
+values('stelian.ardelean@ti.utm.md', 'zxcvb1234', 'Stelian Ardelean', 2, 1);
+
+INSERT userstable(UserEmail, UserPassword, UserName, UserGroupId, RegControl)
+values('adrian.mihalache@ti.utm.md', 'zxcvb1234', 'Adrian Mihalache', 2, 1);
+
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 1, 'Matematica Superiora-1', 7, 8, 7, ((7+8+7)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 2, 'Matematica Superiora-1', 8, 8, 8, ((8+8+8)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 3, 'Matematica Superiora-1', 8, 8, 7, ((8+8+7)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 4, 'Matematica Superiora-1', 7, 7, 7, ((7+7+7)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 5, 'Matematica Superiora-1', 6, 7, 6, ((6+7+6)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 6, 'Matematica Superiora-1', 7, 6, 7, ((7+6+7)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 7, 'Matematica Superiora-1', 5, 5, 5, ((5+5+5)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 8, 'Matematica Superiora-1', 8, 8, 9, ((8+8+9)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 9, 'Matematica Superiora-1', 10, 10, 9, ((10+10+9)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 10, 'Matematica Superiora-1', 6, 7, 7, ((6+7+7)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 11, 'Matematica Superiora-1', 8, 7, 6, ((8+7+6)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 12, 'Matematica Superiora-1', 6, 6, 6, ((6+6+6)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 1, 'Structuri de date si algoritme', 8, 8, 9, ((8+8+9)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 2, 'Structuri de date si algoritme', 8, 8, 8, ((8+8+8)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 3, 'Structuri de date si algoritme', 8, 8, 7, ((8+8+7)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 4, 'Structuri de date si algoritme', 9, 7, 8, ((9+7+8)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 5, 'Structuri de date si algoritme', 7, 7, 6, ((7+7+6)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 6, 'Structuri de date si algoritme', 7, 6, 7, ((7+6+7)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 7, 'Structuri de date si algoritme', 5, 5, 5, ((5+5+5)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 8, 'Structuri de date si algoritme', 8, 8, 9, ((8+8+9)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 9, 'Structuri de date si algoritme', 10, 10, 9, ((10+10+9)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 10, 'Structuri de date si algoritme', 6, 7, 7, ((6+7+7)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 11, 'Structuri de date si algoritme', 8, 7, 6, ((8+7+6)/3));
+
+insert academicperformance(UserId, ObjectName, Midterm1, Midterm2, Exam, total)
+values( 12, 'Structuri de date si algoritme', 6, 6, 6, ((6+6+6)/3));
+
+insert teachertable(TeacherEmail, TeacherName)
+values('elena.cojuhari@mate.utm.md','Elena Cojuhari');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('victor.orlov@mate.utm.md','Victor Orlov');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('sergiu.corlat@isa.utm.md','Sergiu Corlat');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('kulev.mihail@ia.utm.md','Kulev Mihail');
+
+
+insert teachertable(TeacherEmail, TeacherName)
+values('serghei.lealin@isa.utm.md','Serghei Lealin');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('elena.gogoi@faf.utm.md','Elena Gogoi');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('mihai.gaidau@isa.utm.md','Mihai Gaidau');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('roman.ghica@isa.utm.md','Roman Ghica');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('vasilii.braga@ati.utm.md','Vasilii Braga');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('alexandr.vdovicenco@faf.utm.md','Vdovicenco Alexandr');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('mariana.catruc@fcim.utm.md','Mariana Catruc');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('cojuhari.irina@ati.utm.md','Cojuhari Irina');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(1, 2, 'LUN', '8:00', 'lab. Teh. Mult.', 5, '3-118');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(1, 0, 'LUN', '9:45', 'ECD', 6, '3-113');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(1, 1, 'LUN', '9:45', 'lab. C. C.', 7, '3-113');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(1, 2, 'MAR', '8:00', 'c. Etica, Comunicare si Drept', 8, 'online');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(1, 2, 'MAR', '9:45', 'c. Tehnol. Multim.', 9, 'online');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(1, 2, 'MER', '8:00', 'LFPC', 10, '3-113');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(1, 2, 'MER', '9:45', 'lab. C. C.', 7, '3-310');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(1, 2, 'JOI', '8:00', 'c. Calcul si Complexitate', 11, 'online');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(1, 2, 'JOI', '9:45', 'c. LFPC', 12, 'online');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(1, 0, 'VIN', '8:00', 'ECD', 8, '3-104');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(1, 0, 'VIN', '9:45', 'LFPC.', 10, '3-118');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(1, 1, 'VIN', '8:00', 'sem. Teh. Mult.', 5, '3-104');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(1, 1, 'VIN', '9:45', 'ECD', 8, '3-118');
+
+/*-------------------------------------------*/
+insert teachertable(TeacherEmail, TeacherName)
+values('victor.besliu@ati.utm.md','Victor Besliu');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('ionel.antohi@ati.utm.md','Ionel Antohi');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('andrei.nastas@calc.utm.md','Andrei Nastas');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('radu.melnic@adm.utm.md','Radu Melnic');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('dumitru.postavan@isa.utm.md','Dumitru Postavan');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('adrian.secrieru@ati.utm.md','Adrian Secrieru');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('raducanu.octavian@isa.utm.md','Raducanu Octavian');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('nina.sava@ati.utm.md','Nina Sava');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('alexandru.ursu@iis.utm.md','Alexandru Ursu');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('ion.bolun@isa.utm.md','Ion Bolun');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('rusu.corina@fcim.utm.md','Rusu Corina');
+
+insert teachertable(TeacherEmail, TeacherName)
+values('victor.ciorba@dmmc.utm.md','Victor Ciorba');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(2, 2, 'LUN', '9:45', 'c. Prog. Pilot. De Evenim.', 13, 'online');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(2, 2, 'LUN', '13:30', 'c. Tehnologii Web', 14, 'online');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(2, 2, 'MAR', '8:00', 'c. Grafica. pe Calc.', 15, 'online');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(2, 2, 'MAR', '9:45', 'c. AMOO', 16, 'online');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(2, 2, 'MAR', '11:30', 'c. Arhitect. Calcul.', 17, 'online');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(2, 2, 'MER', '8:00', 'lab. A. C.', 18, '3-513');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(2, 2, 'MER', '9:45', 'lab. Retele de Calc.', 19, '3-518');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(2, 0, 'MER', '11:30', 'sem. AMOO', 20, '3-401');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(2, 1, 'MER', '9:45', 'lab. Grafic. pe Calc', 21, '3-513');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(2, 0, 'JOI', '8:00', 'c. AMOO', 16, 'online');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(2, 2, 'JOI', '9:45', 'c. Reţele de Calcul', 22, 'online');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(2, 1, 'JOI', '11:30', 'c. Tehnologii Web', 14, 'online');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(2, 2, 'VIN', '8:00', 'lab. Teh. Web', 23, '3-501');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(2, 2, 'VIN', '9:45', 'lab. AMOO', 20, '3-501');
+
+insert timetable(GroupId, WeekType, Zi, Timp, ObjectName, TeacherId, Location)
+values(2, 2, 'VIN', '11:30', 'lab. PPE', 24, '3-501');
